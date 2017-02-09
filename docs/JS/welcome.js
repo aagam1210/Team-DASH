9811eca6d3e8369c6dcb16b1d04acb553044286f
Aagam Shah <aagam.shah1210@gmail.com>
2017-02-09 17:26:41 -0600

    added css js files
    completed 80% implementation including js for welcome page

---
+++ docs/JS/welcome.js
@@ -0,0 +1,20 @@
+function myD(){
+  searchBar = document.getElementById('searchBar');
+  if(searchBar.value){
+    if(searchBar.value == "Cake"){
+      alert("Your Favourite Dessert is a Cake. It has so much Calories. One piece of Cake has 500 Calories. You are gonna be fat. Please SignUp or Login to track your dessert intake.");
+    }
+    if(searchBar.value == "Chocolate Mousse"){
+      alert("Your Favourite Dessert is a Chocolate Mousse. I love it too. One piece has 300 Calories. Please SignUp or Login to track your dessert intake.");
+    }
+    if(searchBar.value == "Tart"){
+      alert("Your Favourite Dessert is a Tart. It has so much Calories. One piece of Tart has 200 Calories. It's healthy. Please SignUp or Login to track your dessert intake.");
+    }
+    if(searchBar.value == "Ice Cream"){
+      alert("Your Favourite Dessert is a Ice Cream. Yummy!!!. One scope of Ice Cream has 250 Calories. Please SignUp or Login to track your dessert intake.");
+    }
+   if(searchBar.value == "Oreo Shake"){
+      alert("Your Favourite Dessert is a Oreo Shake. Oreo and Milk Shake - an AWESOME Combination. One glass of Oreo Shake has 1000 Calories. You are gonna regret this. Please SignUp or Login to track your dessert intake.");
+    }
+  }
+}
