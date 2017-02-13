0a3f233ed82166cca01049384be300b8eb2cf7e9
Aagam Shah <aagam.shah1210@gmail.com>
2017-02-13 07:04:33 -0600

    UI and JS for Signup
    Updated UI, added JS validation for signup, Profile link on signup
    deactivated.

---
+++ docs/JS/profile.js
@@ -0,0 +1,9 @@
+window.onload () {
+  name = localStorage.getItem("name");
+  email = localStorage.getItem("email");
+  age = localStorage.getItem("age");
+  pnumber = localStorage.getItem("pnumber");
+  gender = localStorage.getItem("gender");
+
+  alert(name);
+}
---
+++ docs/JS/signup.js
@@ -0,0 +1,45 @@
+function validate () {
+	name = document.getElementById('name').value;
+	email = document.getElementById('email').value;
+	pnumber = document.getElementById('pnumber').value;
+	age = document.getElementById('age').value;
+	uname = document.getElementById('uname').value;
+	passwd = document.getElementById('passwd').value;
+	repasswd = document.getElementById('re-passwd').value;
+	var atposition = email.indexOf("@");
+	var dotposition = email.lastIndexOf(".");
+	gender = document.getElementById("gender").value;
+
+	if(name == "" || email == "" || age == "" || pnumber == "" || uname == "" || passwd == "" || repasswd == "") {
+		alert("Please enter all required fields");
+		flag=false;
+	}
+	else if (atposition < 1 || dotposition < atposition+2 || dotposition+2 >= email.length) {  
+  		alert("Please enter a valid e-mail address");
+  		flag=false;
+  	}
+  	else if(isNaN(age) || age < 0 || age > 99) {
+  		alert("Please enter valid age");
+  		flag=false;
+  	}
+	else if(isNaN(pnumber) || pnumber < 1000000000 || pnumber > 9999999999) {
+  		alert("Please enter valid phone number");
+  		flag=false;
+  	}
+  	else if(passwd.length < 6) {
+  		alert("Please enter minimum 6 character password");
+  		flag=false;
+  	}
+	else if(passwd != repasswd) {
+  		alert("The entered passwords do not match");
+  		flag=false;
+  	}
+  	else {
+  		myStorage.setItem("name", name);
+  		myStorage.setItem("email", email);
+  		myStorage.setItem("age", age);
+  		myStorage.setItem("pnumber", pnumber);
+  		myStorage.setItem("gender", name);
+  		window.location.href="../HTML/profile.html";
+  	}
+}
