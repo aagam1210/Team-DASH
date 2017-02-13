7ec1b1325609001db4d4b064516453c28eb1bb69
Amrita Das <amritadas2017@u.northwestern.edu>
2017-02-13 17:24:01 -0600

    Adding the else statement

--- docs/JS/welcome.js
+++ docs/JS/welcome.js
@@ -11,10 +11,11 @@ function myD(){
       alert("Your Favourite Dessert is a Tart. It has so much Calories. One piece of Tart has 200 Calories. It's healthy. Please SignUp or Login to track your dessert intake.");
     }
     if(searchBar.value == "Ice Cream"){
-      alert("Your Favourite Dessert is a Ice Cream. Yummy!!!. One scope of Ice Cream has 250 Calories. Please SignUp or Login to track your dessert intake.");
+      alert("Your Favourite Dessert is a Ice Cream. Yummy!!!. One scoop of Ice Cream has 250 Calories. Please SignUp or Login to track your dessert intake.");
     }
    if(searchBar.value == "Oreo Shake"){
       alert("Your Favourite Dessert is a Oreo Shake. Oreo and Milk Shake - an AWESOME Combination. One glass of Oreo Shake has 1000 Calories. You are gonna regret this. Please SignUp or Login to track your dessert intake.");
     }
+      alert("Your Favourite Dessert is a" + searchBar.value + ". It has 350 calories. Please SignUp or Login to track your dessert intake.");
   }
 }
